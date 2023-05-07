e9bd098e9ecc5638a378accacc839d76ef2056b7
tointhun <132234362+tointhun@users.noreply.github.com>
2023-05-07 16:55:06 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-let os, app;
 let ua = navigator.userAgent;
-let notification;
+let os, app, browser, sub;
+
 
 if(window.navigator.standalone){
     app = "pwa";
 }else{
     app = "browser";
 }
+
 
 if(ua.indexOf("iPhone") > 0){
     os = "ios";
@@ -20,14 +21,79 @@ if(ua.indexOf("iPhone") > 0){
     os = "other";
 }
 
-OneSignal.push(() => {
-    OneSignal.getNotificationPermission((permission) => {
-        if(permission == "granted"){
-            notification = "allowed";
-        }else{
-            notification = "forbided";
-        }
-    })
-})
 
-alert(os + app + notification);
+if(window.navigator.userAgent.toLowerCase().indexOf("safari") != -1 && window.navigator.userAgent.indexOf("GSA") == -1){
+    browser = "safari";
+}else if(window.navigator.userAgent.toLowerCase().indexOf("safari") != -1 && window.navigator.userAgent.indexOf("GSA") != -1){
+    browser = "google app"
+}else{
+    browser = "other"
+}
+
+
+if(document.getElementsByClassName("onesignal-reset onesignal-customlink-subscribe medium button state-unsubscribed").length == 1){
+    sub = "unsub";
+}else if(document.getElementsByClassName("onesignal-reset onesignal-customlink-subscribe medium button state-subscribed").length == 1){
+    sub = "sub";
+}else{
+    sub = "unsub";
+}
+
+
+
+const show_page = (page) =>{
+    let pages = ["middle_index","middle_sub"];
+    for(let i = 0; i < pages.length; i++){
+        if(page != pages[i]){
+            document.getElementById(pages[i]).style.display = "none";
+        }else{
+            document.getElementById(pages[i]).style.display = "block";
+        }
+    }
+}
+
+
+if(os == "ios"){
+    if(app == "pwa"){
+        if(sub == "sub"){
+            show_page("middle_index");
+        }else if(sub == "unsub"){
+            show_page("middle_sub");
+        }
+    }else if(app == "browser"){
+        if(browser == "safari"){
+            //add_home.html
+        }else if(browser == "google app"){
+            //tosafari_fromgoogle.html
+        }else if(browser == "other"){
+            //tosafari.html
+        }
+    }
+}else if(os == "android"){
+    if(app == "pwa"){
+        if(sub == "sub"){
+            //index.html
+        }else if(sub == "unsub"){
+            //sub.html
+        }
+    }else if(app == "browser"){
+        //add.html
+    }
+}else{
+    if(sub == "sub"){
+        //index.html
+    }else if(sub == "unsub"){
+        //sub.html
+    }
+}
+
+
+
+
+const sub_button = () => {
+    if(document.getElementsByClassName("onesignal-reset onesignal-customlink-subscribe medium button state-unsubscribed").length == 1){
+        document.getElementsByClassName("onesignal-reset onesignal-customlink-subscribe medium button state-unsubscribed").click()
+    }else if(document.getElementsByClassName("onesignal-reset onesignal-customlink-subscribe medium button state-subscribed").length == 1){
+        document.getElementsByClassName("onesignal-reset onesignal-customlink-subscribe medium button state-subscribed").click()
+    }
+}
