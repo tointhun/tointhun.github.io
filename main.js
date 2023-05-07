01f982907e63f1dc732f12bb75023bfc34309c4f
tointhun <132234362+tointhun@users.noreply.github.com>
2023-05-07 17:53:37 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,36 @@ let ua = navigator.userAgent;
 let os, app, browser, sub;
 
 
+if(window.navigator.standalone){
+    app = "pwa";
+}else{
+    app = "browser";
+}
+
+
+if(ua.indexOf("iPhone") > 0){
+    os = "ios";
+}else if(ua.indexOf("iPad") > 0){
+    os = "ios";
+}else if(ua.indexOf("iPod") > 0){
+    os = "ios";
+}else if(ua.indexOf("Android") > 0){
+    os = "android";
+}else{
+    os = "other";
+}
+
+
+if(window.navigator.userAgent.toLowerCase().indexOf("safari") != -1 && window.navigator.userAgent.indexOf("GSA") == -1){
+    browser = "safari";
+}else if(window.navigator.userAgent.toLowerCase().indexOf("safari") != -1 && window.navigator.userAgent.indexOf("GSA") != -1){
+    browser = "google app"
+}else{
+    browser = "other"
+}
+
+
+
 
 const show_page = (page) =>{
     let pages = ["middle_index","middle_sub"];
@@ -14,101 +44,63 @@ const show_page = (page) =>{
     }
 }
 
-window.OneSignal = window.OneSignal || [];
-OneSignal.push(function() {
-    OneSignal.init({
-        appId: "301acfc8-b601-457e-a6c5-a5f21d87edbb",
-    });
+async function checkNotificationPermission(){
+    const permission = await OneSignal.getNotificationPermission();
+    if (permission === "granted") {
+        sub = "sub";
+    } else {
+        sub = "unsub"
+    }
+    alert(sub);
+}
 
-    OneSignal.isPushNotificationsEnabled(function(isEnabled) {
-        if(isEnabled){
-            sub = "sub";
-        }else{
-            unsub = "unsub";
-        }
+checkNotificationPermission();
 
+window.onload = (event) => {
+    alert(os + "," + app + "," + browser + "," + sub)
 
-        if(window.navigator.standalone){
-            app = "pwa";
-        }else{
-            app = "browser";
-        }
-        
-
-        if(ua.indexOf("iPhone") > 0){
-            os = "ios";
-        }else if(ua.indexOf("iPad") > 0){
-            os = "ios";
-        }else if(ua.indexOf("iPod") > 0){
-            os = "ios";
-        }else if(ua.indexOf("Android") > 0){
-            os = "android";
-        }else{
-            os = "other";
-        }
-        
-        
-        if(window.navigator.userAgent.toLowerCase().indexOf("safari") != -1 && window.navigator.userAgent.indexOf("GSA") == -1){
-            browser = "safari";
-        }else if(window.navigator.userAgent.toLowerCase().indexOf("safari") != -1 && window.navigator.userAgent.indexOf("GSA") != -1){
-            browser = "google app"
-        }else{
-            browser = "other"
-        }
-        
-
-        
-        window.onload = (event) => {
-            alert(os,app,browser,sub);
-        
-            if(os == "ios"){
-                if(app == "pwa"){
-                    if(sub == "sub"){
-                        show_page("middle_index");
-                    }else if(sub == "unsub"){
-                        show_page("middle_sub");
-                    }
-                }else if(app == "browser"){
-                    if(browser == "safari"){
-                        //add_home.html
-                    }else if(browser == "google app"){
-                        //tosafari_fromgoogle.html
-                    }else if(browser == "other"){
-                        //tosafari.html
-                    }
-                }
-            }else if(os == "android"){
-                if(app == "pwa"){
-                    if(sub == "sub"){
-                        //index.html
-                    }else if(sub == "unsub"){
-                        //sub.html
-                    }
-                }else if(app == "browser"){
-                    //add.html
-                }
-            }else{
-                if(sub == "sub"){
-                    //index.html
-                }else if(sub == "unsub"){
-                    //sub.html
-                }
+    if(os == "ios"){
+        if(app == "pwa"){
+            if(sub == "sub"){
+                show_page("middle_index");
+            }else if(sub == "unsub"){
+                show_page("middle_sub");
+            }
+        }else if(app == "browser"){
+            if(browser == "safari"){
+                //add_home.html
+            }else if(browser == "google app"){
+                //tosafari_fromgoogle.html
+            }else if(browser == "other"){
+                //tosafari.html
             }
         }
-    });
-});
-
-
-
+    }else if(os == "android"){
+        if(app == "pwa"){
+            if(sub == "sub"){
+                //index.html
+            }else if(sub == "unsub"){
+                //sub.html
+            }
+        }else if(app == "browser"){
+            //add.html
+        }
+    }else{
+        if(sub == "sub"){
+            //index.html
+        }else if(sub == "unsub"){
+            //sub.html
+        }
+    }
+}
 
 
 
 
 const sub_button = () => {
-    console.log("a")
     if(document.getElementsByClassName("onesignal-reset onesignal-customlink-subscribe medium button state-unsubscribed").length == 1){
-        document.getElementsByClassName("onesignal-reset onesignal-customlink-subscribe medium button state-unsubscribed")[0].click()
+        document.getElementsByClassName("onesignal-reset onesignal-customlink-subscribe medium button state-unsubscribed").click()
     }else if(document.getElementsByClassName("onesignal-reset onesignal-customlink-subscribe medium button state-subscribed").length == 1){
-        document.getElementsByClassName("onesignal-reset onesignal-customlink-subscribe medium button state-subscribed")[0].click()
+        document.getElementsByClassName("onesignal-reset onesignal-customlink-subscribe medium button state-subscribed").click()
     }
 }
